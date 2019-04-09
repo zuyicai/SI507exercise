81db26acf1996df705549fc71c1befe552f2ef27
zoey <zuyicai@umich.edu>
2019-04-09 11:17:01 -0400

    add comments and json module

--- prog.py
+++ prog.py
@@ -1,9 +1,12 @@
-import random
+import random,json
+
 
 def num_vowels(string):
     count =0
     vowels = ['a','e','i','o','u']
+    # given the vowels
     for i in range((len(string))):
         if string[i].lower in vowels:
             count +=1
     return count
+    # return the number of vowels in the string
